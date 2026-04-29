9e0d78c71fa2ab29c81aa42fe47b2e4163270440
matrixjoeq <momentumrotation@163.com>
2026-04-29 17:32:22 +0800

    enrich r statistics result

--- src/etf_momentum/analysis/r_multiple.py
+++ src/etf_momentum/analysis/r_multiple.py
@@ -348,7 +348,7 @@ def _parse_trade_ts(v: Any) -> pd.Timestamp:
 
 
 def _mfe_default_thresholds() -> list[float]:
-    return [1.0, 1.5, 2.0, 2.5, 3.0, 4.0]
+    return [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
 
 
 def _threshold_key(x: float) -> str:
